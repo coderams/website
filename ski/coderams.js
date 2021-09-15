28f31575fa831ee50e767b9133b21354bc9775ce
plourde27 <48059458+plourde27@users.noreply.github.com>
2021-09-15 01:38:30 -0700

    bugfix

--- ski/coderams.js
+++ ski/coderams.js
@@ -2400,7 +2400,7 @@ function runProgram(num, trailInd, raceInd) {
       if (raceInd != -1) {
         document.getElementById("top").hidden = false;
         if (starting > 16) {
-          document.getElementById("top").innerHTML = "UPDATE8 Instructions: go to the right of the red slalom poles, and to the left of the blue ones";
+          document.getElementById("top").innerHTML = "UPDATE9 Instructions: go to the right of the red slalom poles, and to the left of the blue ones";
         }
         else if (starting > 11) {
           document.getElementById("top").innerHTML = "Your goal is to reach the finish line as fast as possible";
